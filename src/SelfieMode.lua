7802e560ff1f7824e6cb288604da53859106a124
Byran <bemery2@missouriwestern.edu>
2022-11-18 01:44:01 -0600

    Change CloseButtonPosition to CloseButtonOffset

--- src/SelfieMode.lua
+++ src/SelfieMode.lua
@@ -7,7 +7,7 @@ export type SelfieConfiguration = {
     CameraButtonIcon: string?;
     CloseAfterShotTaken: boolean?;
     CameraButtonPosition: UDim2?;
-    CloseButtonPosition: UDim2?;
+    CloseButtonOffset: UDim2?;
 };
 
 export type SelfieMode = {
@@ -24,7 +24,7 @@ return function(Configuration: SelfieConfiguration?): SelfieMode
 
     local OverlayObject = Configuration.OverlayObject;
     ScreenshotHud.CameraButtonPosition = Configuration.CameraButtonPosition or UDim2.fromScale(.5, .95);
-    ScreenshotHud.CloseButtonPosition = Configuration.CloseButtonPosition or ScreenshotHud.CameraButtonPosition - UDim2.fromScale(0.075, 0);
+    ScreenshotHud.CloseButtonPosition = ScreenshotHud.CameraButtonPosition - (Configuration.CloseButtonOffset or UDim2.fromScale(0.035, 0));
     ScreenshotHud.CloseWhenScreenshotTaken = if Configuration.CloseAfterShotTaken ~= nil then Configuration.CloseAfterShotTaken else true; -- Defaults to true
 
     local CoreGuiTypes = {};
@@ -59,8 +59,9 @@ return function(Configuration: SelfieConfiguration?): SelfieMode
     table.insert(Connections, RunService.RenderStepped:Connect(function()
         if OverlayObject then
             local Offset = OverlayObject.AbsoluteSize / 2;
-			local Position = OverlayObject.AbsolutePosition + Offset;
-			ScreenshotHud.CameraButtonPosition = UDim2.fromOffset(Position.X, Position.Y);
+            local Position = OverlayObject.AbsolutePosition + Offset;
+            ScreenshotHud.CameraButtonPosition = UDim2.fromOffset(Position.X, Position.Y);
+            ScreenshotHud.CloseButtonPosition = ScreenshotHud.CameraButtonPosition - (Configuration.CloseButtonOffset or UDim2.fromScale(0.035, 0));
         end
     end))
 
